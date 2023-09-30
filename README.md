2855822a7919f90c11fd403e25fef6c9eeef7432
Stanislav <stas.seryogin.87@gmail.com>
2023-09-30 07:22:31 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# choice_date_of_birth
+ 
---
+++ android/app/src/main/kotlin/com/example/choice_date_of_birth/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.choice_date_of_birth
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
